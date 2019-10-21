96691ccf9973289a2f1e892628a28dd34f2df502
sqwezzy <maks.subbotko46@gmail.com>
2019-10-21 14:05:38 +0300

    Update to complite object

--- object.test.js
+++ object.test.js
@@ -2,8 +2,9 @@ const PUT_ANSWER_HERE = Symbol();
 
 describe('Objects', () => {
   it('Should get the value at path of object. If the resolved value is undefined, the defaultValue is returned in its place.', () => {
-    function get(obj, path) {}
-
+      function get(obj, path) {
+          return path.split('.').reduce((item, cur) => item[cur], obj)
+      }
     expect(get({ a: { b: { c: 3 } } }, 'a')).toStrictEqual({ b: { c: 3 } });
     expect(get({ a: { b: { c: 3 } } }, 'a.b.c')).toBe(3);
     expect(get({ a: { b: { c: 1, d: 2 } } }, 'a.b')).toStrictEqual({
@@ -13,10 +14,11 @@ describe('Objects', () => {
   });
 
   it('Creates an object composed of the picked object properties.', () => {
-    function pick(obj, props) {
-      let newObject = {props};
-      return
-    }
+      function pick(obj, props) {
+        let newObject = {};
+        props.reduce((item, prop) => newObject[prop] = obj[prop], obj);
+          return newObject;
+      }
     const object = { a: 1, b: '2', c: 3 };
 
     expect(pick(object, ['a', 'c'])).toStrictEqual({ a: 1, c: 3 });
@@ -29,12 +31,17 @@ describe('Objects', () => {
       secondName: 'Ivanov'
     };
 
-    const person2 = person1;
+    const person2 = Object.assign({},person1);
     person2.firstName += ' Jr.';
+
+      const person3 = {...person1};
+      person3.firstName += ' Jr.';
 
     expect(person1.firstName).toBe('Ivan');
     expect(person2.firstName).toBe('Ivan Jr.');
     expect(person2.secondName).toBe('Ivanov');
+    expect(person3.secondName).toBe('Ivanov');
+    expect(person3.firstName).toBe('Ivan Jr.');
   });
 
   it('Performs a shallow comparison between two values to determine if they are equivalent.', () => {
@@ -42,24 +49,43 @@ describe('Objects', () => {
     const obj2 = { a: 1, b: 2 };
     const obj3 = { a: 1, b: 4 };
 
-    expect(/* compare(obj1, obj2) */).toBe(true);
-    expect(/* compare(obj1, obj3) */).toBe(false);
+    function compare(obj1, obj2) {
+        for (let key in obj1) {
+            if (obj1[key] !== obj2[key])
+                return false;
+        }
+        return true;
+    }
+
+    expect(compare(obj1, obj2)).toBe(true);
+    expect(compare(obj1, obj3)).toBe(false);
   });
 
   it('Performs a deep comparison between two values to determine if they are equivalent.', () => {
     const obj1 = { a: 1, b: { a: 2 } };
     const obj2 = { a: 1, b: { a: 2 } };
-
-    expect('/* compare(obj1, obj2) */').toBe(true);
+    const obj3 = { a: 2, b: { a: 3 } };
+      const compare = (obj1, obj2) => {
+          let compareValue = typeof obj1 === 'object' && typeof obj2 === 'object';
+          if (!compareValue && obj1 !== obj2) {
+              return false;
+          }
+          for (let key in obj1) {
+              if (typeof obj1[key] !== typeof obj2[key]) {
+                  return false;
+               } else if (!compare(obj1[key], obj2[key])) {
+                   return false;
+               }
+          }
+          return true;
+      };
+    expect(compare(obj1, obj2)).toBe(true);
+    expect(compare(obj1, obj3)).toBe(false);
   });
 
   it('Fix me', () => {
     function hasAccess(role) {
-      if (role == { type: 'admin' }) {
-        return true;
-      } else {
-        return false;
-      }
+        return role.type === 'admin';
     }
 
     expect(hasAccess({ type: 'admin' })).toBe(true);
